9edfc3a832949233088bec225c8c972eec937491
nhduy01 <nhduy2001@gmail.com>
2024-07-23 21:02:42 +0700

    clean topic service

--- Application/Services/TopicService.cs
+++ Application/Services/TopicService.cs
@@ -114,13 +114,6 @@ public class TopicService : ITopicService
     #endregion
 
 
-    //Check Id is Exist
-    public async Task<bool> IsExistedId(Guid id)
-    {
-        return await _unitOfWork.SponsorRepo.IsExistIdAsync(id);
-    }
-
-
     #region Validate
     public async Task<ValidationResult> ValidateTopicRequest(TopicRequest topic)
     {

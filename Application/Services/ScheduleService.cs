9d54248c0779d6767c06a573743b5c9faa41dc3a
NhaPhan <phanthanhnha06@gmail.com>
2024-07-24 17:20:02 +0700

    Fix bug create schedule

--- Application/Services/ScheduleService.cs
+++ Application/Services/ScheduleService.cs
@@ -82,7 +82,7 @@ public class ScheduleService : IScheduleService
         //Get Paintings Of Preliminary round
         var listPainting = await _unitOfWork.RoundTopicRepo.ListPaintingForPreliminaryRound(schedule.RoundId);
         var round = await _unitOfWork.RoundRepo.GetRoundDetail(schedule.RoundId);
-        if (round?.EndTime > DateTime.Today)
+        if (round.Status != RoundStatus.Complete.ToString())
         {
             throw new Exception("Bạn Không Thể Lên Lịch Chấm Khi Cuộc Thi Chưa Kết Thúc");
         }

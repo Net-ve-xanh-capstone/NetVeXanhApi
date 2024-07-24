4a3b41eff1f58721f61d386aec13a4881051bd99
NhaPhan <phanthanhnha06@gmail.com>
2024-07-25 01:06:12 +0700

    add missing field

--- Application/ViewModels/ScheduleViewModels/ScheduleViewModel.cs
+++ Application/ViewModels/ScheduleViewModels/ScheduleViewModel.cs
@@ -4,6 +4,7 @@ public class ScheduleViewModel
 {
     public Guid Id { get; set; }
     public Guid? RoundId { get; set; }
+    public string? Description { get; set; }
     public string? Round { get; set; }
     public string? Year { get; set; }
     public Guid? ExaminerId { get; set; }

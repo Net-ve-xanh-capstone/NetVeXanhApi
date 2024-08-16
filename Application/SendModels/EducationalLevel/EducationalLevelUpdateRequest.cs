bfbe5c60082be247e3612a1adeb9f1bef993f4b1
nhduy01 <nhduy2001@gmail.com>
2024-08-16 17:11:31 +0700

    update level

--- Application/SendModels/EducationalLevel/EducationalLevelUpdateRequest.cs
+++ Application/SendModels/EducationalLevel/EducationalLevelUpdateRequest.cs
@@ -5,7 +5,6 @@ public class EducationalLevelUpdateRequest
     public Guid Id { get; set; }
     public string? Level { get; set; }
     public string? Description { get; set; }
-    public Guid? ContestId { get; set; }
     public int? MinAge { get; set; }
     public int? MaxAge { get; set; }
     public Guid CurrentUserId { get; set; }

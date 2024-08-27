423140aaba4636112c7485d9d00c152c111ea0b1
nhduy01 <nhduy2001@gmail.com>
2024-08-28 04:56:28 +0700

    fix create account

--- WebAPI/Validation/AccountValidation/CreateAccountRequestValidator.cs
+++ WebAPI/Validation/AccountValidation/CreateAccountRequestValidator.cs
@@ -45,8 +45,7 @@ public class CreateAccountRequestValidator : AbstractValidator<CreateAccountRequ
             .MinimumLength(8).WithMessage("Mật khẩu phải có ít nhất 8 ký tự.")
             .Matches("[A-Z]").WithMessage("Mật khẩu phải chứa ít nhất một chữ cái viết hoa.")
             .Matches("[a-z]").WithMessage("Mật khẩu phải chứa ít nhất một chữ cái viết thường.")
-            .Matches("[0-9]").WithMessage("Mật khẩu phải chứa ít nhất một chữ số.")
-            .Matches("[^a-zA-Z0-9]").WithMessage("Mật khẩu phải chứa ít nhất một ký tự đặc biệt.");
+            .Matches("[0-9]").WithMessage("Mật khẩu phải chứa ít nhất một chữ số.");
 
         RuleFor(user => user.Phone)
             .Must(phone => !string.IsNullOrEmpty(phone) && Regex.IsMatch(phone, @"^0\d{9}$"))

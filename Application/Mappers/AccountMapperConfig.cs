1763d3e82f4f1671c2ce2eeb6e84549d1d84136e
nhduy01 <nhduy2001@gmail.com>
2024-09-15 15:18:26 +0700

    change status competitor in round to vietnamese

--- Application/Mappers/AccountMapperConfig.cs
+++ Application/Mappers/AccountMapperConfig.cs
@@ -53,7 +53,19 @@ public partial class MapperConfigs : Profile
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => CalculateAge(src.Birthday!.Value)))
             .ForMember(dest => dest.Prize, opt => opt.Ignore())
             .ForMember(dest => dest.RoundName, opt => opt.Ignore())
-            .ForMember(dest => dest.Status, opt => opt.Ignore());
+            .ForMember(dest => dest.Status, opt => opt.Ignore())
+            .ForPath(dest => dest.Status, opt => opt.MapFrom(src =>
+                src.Status == PaintingStatus.Draft.ToString() ? "Bản nháp" :
+                src.Status == PaintingStatus.Submitted.ToString() ? "Đã nộp" :
+                src.Status == PaintingStatus.Delete.ToString() ? "Đã xóa" :
+                src.Status == PaintingStatus.Accepted.ToString() ? "Đã chấp nhận" :
+                src.Status == PaintingStatus.Rejected.ToString() ? "Đã từ chối" :
+                src.Status == PaintingStatus.Pass.ToString() ? "Qua Vòng" :
+                src.Status == PaintingStatus.NotPass.ToString() ? "Không qua vòng" :
+                src.Status == PaintingStatus.FinalRound.ToString() ? "Vòng chung kết" :
+                src.Status == PaintingStatus.HasPrizes.ToString() ? "Có giải thưởng" :
+                "Trạng thái không xác định"
+            ));
     }
 
     private int CalculateAge(DateTime birthday)

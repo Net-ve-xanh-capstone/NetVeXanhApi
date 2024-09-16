c5875f18ab3ea93ba4383a61db2ec368d2add2e4
nhduy01 <nhduy2001@gmail.com>
2024-09-16 21:43:28 +0700

    fix get award when create schedule

--- Infracstructures/Repositories/AwardRepository.cs
+++ Infracstructures/Repositories/AwardRepository.cs
@@ -23,9 +23,20 @@ public class AwardRepository : GenericRepository<Award>, IAwardRepository
 
     public async Task<List<Award>?> GetAwardsByRoundId(Guid roundId)
     {
-        return await DbSet
+        var awards = await DbSet
             .Include(x => x.AwardSchedule)
-            .Where(x => x.RoundId == roundId && x.Status == AwardStatus.Active.ToString())
+            .Where(x => x.RoundId == roundId
+                        && x.Status == AwardStatus.Active.ToString())
             .ToListAsync();
+
+        // Lọc AwardSchedule với điều kiện Status là Active
+        foreach (var award in awards)
+        {
+            award.AwardSchedule = award.AwardSchedule
+                .Where(schedule => schedule.Status == AwardStatus.Active.ToString())
+                .ToList();
+        }
+
+        return awards;
     }
 }

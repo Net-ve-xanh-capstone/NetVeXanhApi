60222cb034ff17973ef37b35c42e6146b11e71ad
nhduy01 <nhduy2001@gmail.com>
2024-09-26 15:28:25 +0700

    sort filter painting

--- Infracstructures/Repositories/PaintingRepository.cs
+++ Infracstructures/Repositories/PaintingRepository.cs
@@ -156,7 +156,7 @@ public class PaintingRepository : GenericRepository<Painting>, IPaintingReposito
 
         if (!string.IsNullOrEmpty(filterPainting.Status)) query = query.Where(p => p.Status == filterPainting.Status);
 
-        return query.OrderBy(x=>x.CreatedTime).ToList();
+        return query.OrderByDescending(x=>x.SubmittedTimestamp).ToList();
     }
 
 
